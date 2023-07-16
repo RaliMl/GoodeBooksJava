c43b9faf72da823d41e45f11c4b6b63a83929856
RaliMl <ralitsamladenova03@gmail.com>
2023-07-16 14:32:08 +0300

    Initial commit

---
+++ src/main/java/com/bookstore/demo/GoodeBooksJavaApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodeBooksJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodeBooksJavaApplication.class, args);
+	}
+
+}
