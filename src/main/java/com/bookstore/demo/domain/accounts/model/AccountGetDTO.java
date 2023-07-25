4c42f76228e5a35fef857e497992f328bbf60a53
RaliMl <ralitsamladenova03@gmail.com>
2023-07-25 11:40:52 +0300

    fixDtos

--- src/main/java/com/bookstore/demo/domain/accounts/model/AccountGetDTO.java
+++ src/main/java/com/bookstore/demo/domain/accounts/model/AccountGetDTO.java
@@ -1,14 +1,8 @@
 package com.bookstore.demo.domain.accounts.model;
 
-import javax.validation.constraints.*;
-
 public record AccountGetDTO(
         Long id,
-        @Email(message = "The entered email address is invalid!")
-        @NotBlank
         String email,
-        @NotBlank
-        @Pattern(regexp = "(?=.*[A-Z])(?=.*[a-z])(?=.*[-'])*.{3,}", message = "The entered name is invalid!")
         String fullName,
         Long role
 ) {
